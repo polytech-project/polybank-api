b8635b1b8a7a4f7688fe1683429ba4f8b6d07fcb
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-17 17:13:02 +0100

    feat(architect): add new app's and domain's

--- config/architect.ts
+++ config/architect.ts
@@ -1,11 +1,15 @@
 import { ArchitectConfigContract } from "services/architect/contracts/architect_config_contract";
 
 const architectConfig: ArchitectConfigContract = {
-  domains: ['users'],
+  domains: ['users', 'projects'],
   applications: {
     authentication: {
       prefix: 'authentication',
       as: 'authentication',
+    },
+    projects: {
+      prefix: 'projects',
+      as: 'projects',
     }
   }
 }

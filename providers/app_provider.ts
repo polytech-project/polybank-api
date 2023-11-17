405744c1b0bb392b16e7eaa3efded5e8d456e50f
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-17 09:09:32 +0100

    update `app_provider`

---
+++ providers/app_provider.ts
@@ -0,0 +1,29 @@
+import type { ApplicationContract } from '@ioc:Adonis/Core/Application'
+
+export default class AppProvider {
+  constructor (protected app: ApplicationContract) {
+  }
+
+  public async register () {
+    const { default: Architect } = await import('Services/architect')
+
+    this.app.container.singleton('Adonis/Addons/Architect', () => {
+      return new Architect(this.app)
+    })
+  }
+
+  public async boot () {
+    const architect = this.app.container.resolveBinding('Adonis/Addons/Architect')
+    architect.registerDomains()
+
+    await architect.registerRoutes()
+  }
+
+  public async ready () {
+    // App is ready
+  }
+
+  public async shutdown () {
+    // Cleanup, since app is going down
+  }
+}

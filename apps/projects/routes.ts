367372d9810d2ab5a5416f1e983f33c4caa12f5b
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-19 11:30:15 +0100

    feat(routes): init route for `projects`

--- apps/projects/routes.ts
+++ apps/projects/routes.ts
@@ -4,7 +4,14 @@ export default () => {
 
   Route.group(() => {
     Route.group(() => {
-
+      Route.group(() => {
+        Route.get('/', 'projects_controller.index').as('projects.index')
+        Route.get('/:id', 'projects_controller.show').as('projects.show')
+        
+        Route.post('/', 'projects_controller.store').as('projects.store')
+        Route.put('/:id', 'projects_controller.update').as('projects.update')
+        Route.delete('/:id', 'projects_controller.destroy').as('projects.destroy')
+      }).prefix('/projects')
     }).middleware('auth')
   }).namespace('App/projects/controllers')
 }

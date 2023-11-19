b08512efcec9a4a17d318478e25f27615e9071e5
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-19 11:29:21 +0100

    feat(model): init permission model

---
+++ domains/users/models/permission.ts
@@ -0,0 +1,26 @@
+import {BaseModel, beforeCreate, column, ManyToMany, manyToMany} from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
+import { randomUUID } from 'crypto'
+import Role from 'Domains/users/models/role'
+
+export default class Permission extends BaseModel {
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public identifier: string
+
+  @manyToMany(() => Role)
+  public roles: ManyToMany<typeof Role>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeCreate()
+  public static async generateUuid (model: Permission) {
+    model.id = randomUUID()
+  }
+}

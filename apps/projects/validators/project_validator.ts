6ad61dfaad4880ab136487fdbedca1f048fc4350
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-19 14:10:48 +0100

    feat(validator): add update validator for project

--- apps/projects/validators/project_validator.ts
+++ apps/projects/validators/project_validator.ts
@@ -1,5 +1,5 @@
 import { type HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 
 export class StoreValidator {
   constructor (protected ctx: HttpContextContract) {}
@@ -8,6 +8,23 @@ export class StoreValidator {
     title: schema.string({ trim: true, escape: true }),
     description: schema.string.optional({ trim: true, escape: true }),
     device: schema.string.optional({ trim: true, escape: true }),
+  })
+
+  public messages: CustomMessages = {}
+}
+
+
+export class UpdateValidator {
+  constructor (protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    title: schema.string.optional({ trim: true, escape: true }),
+    description: schema.string.optional({ trim: true, escape: true }),
+    users: schema.array.optional().members(
+      schema.string({ trim: true, escape: true }, [
+        rules.exists({ table: 'users', column: 'id' })
+      ])
+    ),
   })
 
   public messages: CustomMessages = {}

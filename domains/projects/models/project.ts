bf7a15767f7c97a3064c3b16eb828abb7f997d5a
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-19 10:51:37 +0100

    feat(model) init project model

--- domains/projects/models/project.ts
+++ domains/projects/models/project.ts
@@ -1,10 +1,33 @@
-import { BaseModel, beforeCreate, column } from "@ioc:Adonis/Lucid/Orm"
+import { BaseModel, BelongsTo, HasMany, ManyToMany, beforeCreate, belongsTo, column, hasMany, manyToMany } from "@ioc:Adonis/Lucid/Orm"
+import User from "Domains/users/models/user"
 import { DateTime } from "luxon"
 import { randomUUID } from "node:crypto" 
+import Transaction from "./transaction"
 
 export default class Project extends BaseModel {
   @column({ isPrimary: true })
   public id: string
+
+  @column()
+  public title: string
+
+  @column()
+  public description: string
+
+  @column()
+  public device: string
+
+  @column()
+  public ownerId: string
+
+  @belongsTo(() => User, { localKey: 'owner_id' })
+  public owner: BelongsTo<typeof User>
+
+  @manyToMany(() => User)
+  public users: ManyToMany<typeof User>
+
+  @hasMany(() => Transaction)
+  public transactions: HasMany<typeof Transaction>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime

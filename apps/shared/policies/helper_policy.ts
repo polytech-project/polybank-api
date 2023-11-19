23fd40923b84f56d638f1a1b8313dd8315a16729
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-19 11:29:05 +0100

    feat() init bouncer

---
+++ apps/shared/policies/helper_policy.ts
@@ -0,0 +1,18 @@
+import { BasePolicy } from "@ioc:Adonis/Addons/Bouncer";
+import User from "Domains/users/models/user";
+
+export default class HelperPolicy extends BasePolicy {
+  public async before(user: User | null): Promise<true | undefined> {
+    if (user) {
+      const permissions: string[] = await HelperPolicy.getPermissions(user)
+      if (permissions.includes('admin')) {
+        return true
+      }
+    }
+  }
+
+  public static async getPermissions(user: User): Promise<string[]> {
+    await user.load('roles', (query) => query.preload('permissions'))
+  }
+
+}

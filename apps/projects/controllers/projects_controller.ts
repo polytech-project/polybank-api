f28db12f3c1f066444ddc8178b4cbcbfdb4b95ba
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-19 12:47:49 +0100

    feat(controllers): add new method for project

--- apps/projects/controllers/projects_controller.ts
+++ apps/projects/controllers/projects_controller.ts
@@ -1,6 +1,8 @@
 import { type HttpContextContract } from "@ioc:Adonis/Core/HttpContext"
 import Project from "Domains/projects/models/project"
 import ProjectService from "../services/project_service"
+import { StoreValidator } from "../validators/project_validator"
+import User from "Domains/users/models/user"
 
 export default class ProjectsController {
   public async index ({ request, response }: HttpContextContract) {
@@ -16,4 +18,31 @@ export default class ProjectsController {
     // bouncer authorize 'projects', 'view' 
     return Project.query()
   }
+
+  public async show ({ params, response }: HttpContextContract) {
+    // bouncer authorize 'projects', 'view'
+    const project = await ProjectService.getProjectById(params.id)
+
+    if (!project) {
+      return response.notFound()
+    }
+
+    return response.send(project)
+  }
+  
+  public async store ({ request, response, auth }: HttpContextContract) {
+    const data = await request.validate(StoreValidator)
+    const user = auth.user as User
+    
+    const project = await ProjectService.createProject({
+      ...data, user
+    })
+
+    if (!project) {
+      return response.badRequest()
+    }
+
+    return response.created(project)
+  }
+
 }

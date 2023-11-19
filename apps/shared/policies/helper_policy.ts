2b78ec79b68aa615f68c55102f11521b68588467
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-19 14:49:06 +0100

    feat(): update HelperPolicy

--- apps/shared/policies/helper_policy.ts
+++ apps/shared/policies/helper_policy.ts
@@ -1,18 +1,29 @@
-import { BasePolicy } from "@ioc:Adonis/Addons/Bouncer";
-import User from "Domains/users/models/user";
+import { BasePolicy } from '@ioc:Adonis/Addons/Bouncer'
+import User from 'Domains/users/models/user'
 
 export default class HelperPolicy extends BasePolicy {
-  public async before(user: User | null): Promise<true | undefined> {
-    if (user) {
-      const permissions: string[] = await HelperPolicy.getPermissions(user)
-      if (permissions.includes('admin')) {
-        return true
-      }
-    }
-  }
+	public async before(user: User | null): Promise<true | undefined> {
+		if (user) {
+			const permissions: string[] = await HelperPolicy.getPermissions(user)
+			if (permissions.includes('admin')) {
+				return true
+			}
+		}
+	}
 
-  public static async getPermissions(user: User): Promise<string[]> {
-    await user.load('roles', (query) => query.preload('permissions'))
-  }
+	public static async getPermissions(user: User): Promise<string[]> {
+		await user.load('roles', (query) => query.preload('permissions'))
 
+		const permissions: string[] = []
+
+		user.roles.forEach((role) => {
+			role.permissions.forEach((permission) => {
+				if (!permissions.includes(permission.identifier)) {
+					permissions.push(permission.identifier)
+				}
+			})
+		})
+
+		return permissions
+	}
 }

62296e13d5677e1d5cdf8e59a9c21e14ebbe7482
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-27 23:02:18 +0100

    test

---
+++ start/rabbit.ts
@@ -0,0 +1,11 @@
+import Rabbit from "@ioc:Adonis/Addons/Rabbit"
+
+/*
+|--------------------------------------------------------------------------
+| Preloaded File
+|--------------------------------------------------------------------------
+|
+| Any code written inside this file will be executed during the application
+| boot.
+|
+*/
--- start/routes.ts
+++ start/routes.ts
@@ -18,8 +18,25 @@
 |
 */
 
+import Logger from '@ioc:Adonis/Core/Logger'
+import Rabbit from '@ioc:Adonis/Addons/Rabbit'
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
 	return { hello: 'world' }
+})
+
+Route.post('/rabbit', async ({}) => {
+  await Rabbit.assertQueue('test', { durable: false })
+
+  Logger.info('Sending message to queue')
+  const message = {
+    iss: '',
+    sub: 'pro.nathaelbonnal@gmail.com',
+    exp: Math.floor(Date.now() / 1000) + 3600,
+    iat: Math.floor(Date.now() / 1000),
+  }
+
+  await Rabbit.sendToQueue('test', message)
+  return { hello: 'world' }
 })

7acf6a8191a3ea6594d3314224c74b6ff72ad19a
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-27 23:00:47 +0100

    fix(): update table name `role_user`

--- domains/users/migrations/1700388671625_role_users.ts
+++ domains/users/migrations/1700388671625_role_users.ts
@@ -1,7 +1,7 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-	protected tableName = 'role_users'
+	protected tableName = 'role_user'
 
 	public async up() {
 		this.schema.createTable(this.tableName, (table) => {

0d3975e34c4a1205c17f81488ae8942e0b45c429
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-12-10 03:12:03 +0100

    feat(routes): update routes for auth

--- apps/authentication/routes.ts
+++ apps/authentication/routes.ts
@@ -2,9 +2,12 @@ import Route from '@ioc:Adonis/Core/Route'
 
 export default () => {
 	Route.group(() => {
-		Route.get('/:driver/redirect', 'authentication_controller.redirect').as('auth.redirect')
+		Route.get('/:driver/redirect', 'social_authentication_controller.redirect').as('auth.redirect')
 
-		Route.get('/:driver/callback', 'authentication_controller.callback').as('auth.callback')
+		Route.get('/:driver/callback', 'social_authentication_controller.callback').as('auth.callback')
+
+    Route.post('/login', 'authentication_controller.login').as('authentication.login')
+    Route.post('/register', 'authentication_controller.register').as('authentication.register')
 
 		Route.group(() => {
 			Route.get('/me', 'authentication_controller.me').as('auth.me')

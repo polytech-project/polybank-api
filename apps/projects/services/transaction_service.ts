1a2acbf962c0cf9f8992120a0f8f41dedfb2010f
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-12-10 03:09:44 +0100

    feat(services): add `findByProjectId` in transaction_service

--- apps/projects/services/transaction_service.ts
+++ apps/projects/services/transaction_service.ts
@@ -1,24 +1,30 @@
-import Expense from "Domains/projects/models/transaction"
+import Transaction from "Domains/projects/models/transaction"
 //import Logger from '@ioc:Adonis/Core/Logger'
 
 class TransactionService {
   constructor() {}
 
   public async findAll(page: number, size: number) {
-    return Expense.query()
+    return Transaction.query()
       .paginate(page, size)
   }
 
   public async findByUserId(userId: string, page: number, size: number) {
-    return Expense.query()
+    return Transaction.query()
       .where('user_id', userId)
       .paginate(page, size)
   }
 
   public async findById(expenseId: string) {
-    return Expense.query()
+    return Transaction.query()
       .where('id', expenseId)
       .firstOrFail()
+  }
+
+  public async findByProjectId(projectId: string, page: number, size: number) {
+    return Transaction.query()
+      .where('project_id', projectId)
+      .paginate(page, size)
   }
 }
 

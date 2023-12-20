44f3fd57da19a2fa10079003623cb9f90a33eae6
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-12-20 11:46:38 +0100

    update apps shared for middlewares

--- apps/shared/middlewares/set-authorization-header.ts
+++ apps/shared/middlewares/set-authorization-header.ts
@@ -5,6 +5,8 @@ export default class SetAuthorizationHeader {
 		// code for middleware goes here. ABOVE THE NEXT CALL
 		const token = request.cookie('token')
 
+    console.log(token)
+
 		if (token) {
 			request.headers().authorization = `Bearer ${token}`
 		}
@@ -12,3 +14,16 @@ export default class SetAuthorizationHeader {
 		await next()
 	}
 }
+
+
+/**
+ * Mathias paye pour NAthael à 50
+ * -> Mathias +50
+ * -> Nathael -50
+ * -> Dorian 0
+ *
+ *  Dorian paye pour Mathias et Nathael (100€)
+ *  -> Mathias 0
+ *  -> Dorian 100
+ *  -> Nathael -100
+ */

21584de4c2032ccc1924f95a04c1410fdf450cf3
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-12-24 19:57:18 +0100

    update shared app's

--- apps/shared/middlewares/set-authorization-header.ts
+++ apps/shared/middlewares/set-authorization-header.ts
@@ -2,10 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class SetAuthorizationHeader {
 	public async handle({ request }: HttpContextContract, next: () => Promise<void>) {
-		// code for middleware goes here. ABOVE THE NEXT CALL
 		const token = request.cookie('token')
-
-    console.log(token)
 
 		if (token) {
 			request.headers().authorization = `Bearer ${token}`
@@ -14,16 +11,3 @@ export default class SetAuthorizationHeader {
 		await next()
 	}
 }
-
-
-/**
- * Mathias paye pour NAthael à 50
- * -> Mathias +50
- * -> Nathael -50
- * -> Dorian 0
- *
- *  Dorian paye pour Mathias et Nathael (100€)
- *  -> Mathias 0
- *  -> Dorian 100
- *  -> Nathael -100
- */

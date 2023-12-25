b39156f0289eee8bd216763efb5941c4d25ef983
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-12-25 11:38:03 +0100

    fix(security): remove rate limit for /health

--- apps/security/routes.ts
+++ apps/security/routes.ts
@@ -3,6 +3,5 @@ export default () => {
   Route.group(() => {
     Route.get('/health', 'health_controller.check').as('security.health-check')
   })
-    .middleware(['throttle:global'])
     .namespace('App/security/controllers')
 }
